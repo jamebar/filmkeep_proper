d051b5e30a5bb63fbf935d47055acfb458710f47
James Barlow <jbarlow@operationalresults.com>
2015-01-07 09:50:05 -0700

    removed notification from follow

---
+++ app/config/local/packages/get-stream/stream-laravel/config.php
@@ -0,0 +1,49 @@
+<?php
+
+return array(
+
+    /*
+    |-----------------------------------------------------------------------------
+    | Your GetStream.io API credentials (you can them from getstream.io/dashboard)
+    |-----------------------------------------------------------------------------
+    |
+    */
+
+    'api_key' => '4j8dz2dp5vjn',
+    'api_secret' => '3gzp62asbcwjxjyfx7yvrbmctxykwrqc27ypxvnj7xyfu7uygz9rcrdshmvb4fey',
+    'api_site_id' => '1072',
+
+    /*
+    |-----------------------------------------------------------------------------
+    | The default feed manager class
+    |-----------------------------------------------------------------------------
+    |
+    */
+
+    'feed_manager_class' => 'GetStream\StreamLaravel\StreamLaravelManager',
+
+    /*
+    |-----------------------------------------------------------------------------
+    | The feed that keeps content created by its author
+    |-----------------------------------------------------------------------------
+    |
+    */
+    'user_feed' => 'user',
+    /*
+    |-----------------------------------------------------------------------------
+    | The feed containing notification activities
+    |-----------------------------------------------------------------------------
+    |
+    */
+    'notification_feed' => 'notification',
+    /*
+    |-----------------------------------------------------------------------------
+    | The feeds that shows activities from followed user feeds
+    |-----------------------------------------------------------------------------
+    |
+    */
+    'news_feeds' => array(
+        'flat' => 'flat',
+        'aggregated' => 'aggregated',
+    )
+);
