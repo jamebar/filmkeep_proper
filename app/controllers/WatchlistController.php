5283082750a2fda5ea33178cdc7d5dbd060b8242
James Barlow <jbarlow@operationalresults.com>
2015-03-21 23:05:23 -0600

    fixed watchlist page

--- app/controllers/WatchlistController.php
+++ app/controllers/WatchlistController.php
@@ -2,6 +2,7 @@
 
 use Filmkeep\Watchlist;
 use Filmkeep\User;
+use Filmkeep\Review;
 
 class WatchlistController extends \BaseController {
 
@@ -13,11 +14,18 @@ class WatchlistController extends \BaseController {
 	public function index()
 	{
     $user_id = \Input::get('user_id');
-		$watchlist = Watchlist::with('film', 'comments')->where('user_id', $user_id)->orderBy('list_order', 'asc')->orderby('created_at','asc')->get()->toArray();
+		$watchlist = Watchlist::with('film', 'comments')->where('user_id', $user_id)->orderBy('list_order', 'asc')->orderby('created_at','asc')->get();
     
-    foreach($watchlist as &$w){
-      $w['film']['on_watchlist'] = 'true';
+    if($user_id == Auth::user()->id)
+    {
+      foreach($watchlist as &$w){
+        $w['film']['on_watchlist'] = 'true';
+      }
     }
+    else{
+      $this->checkWatchlistReviewed($watchlist);
+    }
+      
     
     return \Response::json(['status' => 200, 'results' => $watchlist]);
 	}
@@ -51,6 +59,29 @@ class WatchlistController extends \BaseController {
 		return Response::json(['action'=>$action]);
 	}
 
+  private function checkWatchlistReviewed($items)
+  {
+      foreach($items as &$item)
+      {
+
+          $review = Review::where('user_id', Auth::user()->id)->whereHas('film', function($q) use ($item)
+          {
+              $q->where('tmdb_id', '=', $item['film']['tmdb_id']);
+
+          })->first();
+          $item['film']['reviewed'] = is_null($review) ? 'false' : 'true';
+
+          
+          $watchlist = Watchlist::where('user_id', Auth::user()->id)->whereHas('film', function($q) use ($item)
+          {
+              $q->where('tmdb_id', '=', $item['film']['tmdb_id']);
+
+          })->first();
+          $item['film']['on_watchlist'] = is_null($watchlist) ? 'false' : 'true';
+       
+      }
+    
+  }
 
 
 }
