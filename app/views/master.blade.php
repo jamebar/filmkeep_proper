e24444c2cdc7638f718feb3b443dd07a5277799c
James Barlow <jbarlow@operationalresults.com>
2015-05-06 21:53:27 -0600

    fix sliders delete bug

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -15,7 +15,7 @@
     <link href='http://fonts.googleapis.com/css?family=Volkhov:400,700italic' rel='stylesheet' type='text/css'>
     <script src="/assets/js/vendor.js?cache=1.2"></script>
     <script id="bundle" src="/assets/js/app-20150410.js"></script>
-    <script id="templates" src="/assets/js/templates-20150502.js"></script>
+    <script id="templates" src="/assets/js/templates-20150506.js"></script>
     <base href="/" />
     <script>
       var image_path_config = {{json_encode($image_path_config)}};
